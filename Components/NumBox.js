09e42d6da9efe9eaa2fab77d2ed0e3fc21b8e4d6
unknown <roshananand2005@gmail.com>
2024-09-01 20:36:53 +0530

    made the animation of the movement

--- Components/NumBox.js
+++ Components/NumBox.js
@@ -3,7 +3,7 @@ import styles from "@/app/The2048/The2048.module.css"
 const NumBox = ({ number,boxId}) => {
     return (
         <>
-            <div className={styles['num-box']}><div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-5xl'>{number}</div></div>
+            <div className={styles['num-box']} id={boxId}><div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-5xl'>{number}</div></div>
         </>
     )
 }
--- app/The2048/page.js
+++ app/The2048/page.js
@@ -7,6 +7,7 @@ import Screen from '@/Components/Screen'
 import NumBox from '@/Components/NumBox'
 import Menu from '@/Components/Menu'
 import { ClientPageRoot } from 'next/dist/client/components/client-page'
+import { BUILD_ID_FILE } from 'next/dist/shared/lib/constants'
 // --bg - color: #FFFAE3;
 // --text - color: #333333;
 // --btn - color: #A8E6CF;
@@ -60,83 +61,93 @@ const page = () => {
     }, [BoxIdArr])
 
     //animating the movement of the box
-    const animateMovement = () => {
-        gsap.to()
+    const animateMovement = (ele, yAxis, xAxis) => {
+        console.log(yAxis*25,xAxis*25);
+        let theBox = document.getElementById(ele);
+        gsap.to(theBox, {
+            position: "relative",
+            top: `${yAxis * 100}%`,
+            left: `${xAxis * 100}%`,
+            duration:0.2
+        })
     }
 
     //funtion to move all the box upwards
-    const moveUp = () => {
+    const moveUp = async () => {
         console.log("move up")
         let tempNumBoxArr = numBoxArr;
         for (let i = 0; i < 4; i++) {
             let space = 0;
             for (let j = 0; j < 4; j++) {
-                let theBox = tempNumBoxArr[tempNumBoxArr.findIndex(pos => pos.x + "" + pos.y == j + "" + i)]
+                let loc = j + "" + i;
+                let theBox = tempNumBoxArr[tempNumBoxArr.findIndex(pos => pos.x + "" + pos.y == loc)]
                 if (theBox) {
-                    console.log(theBox)
                     Object.assign(theBox, { x: theBox.x - space })
-                    console.log(theBox)
+                    animateMovement(loc, -space, 0);
                 } else space++
             }
         }
+        await waitExc(2000)
         setNumBoxArr(tempNumBoxArr);
         setmoved(moved + 1);
     }
 
     //funtion to move all the box downwards
-    const moveDown = () => {
+    const moveDown = async () => {
         console.log("move down")
         let tempNumBoxArr = numBoxArr;
         for (let i = 3; i >= 0; i--) {
             let space = 0;
             for (let j = 3; j >= 0; j--) {
-                let theBox = tempNumBoxArr[tempNumBoxArr.findIndex(pos => pos.x + "" + pos.y == j + "" + i)]
+                let loc = j + "" + i;
+                let theBox = tempNumBoxArr[tempNumBoxArr.findIndex(pos => pos.x + "" + pos.y == loc)]
                 if (theBox) {
-                    console.log(space)
-                    console.log(theBox)
                     Object.assign(theBox, { x: theBox.x + space })
-                    console.log(theBox)
+                    animateMovement(loc, space, 0);
                 } else space++
             }
         }
+        await waitExc(5000)
         setNumBoxArr(tempNumBoxArr);
         setmoved(moved + 1);
     }
 
     //funtion to move all the box to the left
-    const moveLeft = () => {
+    const moveLeft = async () => {
         console.log("move left")
         let tempNumBoxArr = numBoxArr;
         for (let i = 0; i < 4; i++) {
             let space = 0;
             for (let j = 0; j < 4; j++) {
-                let theBox = tempNumBoxArr[tempNumBoxArr.findIndex(pos => pos.x + "" + pos.y == i + "" + j)]
+                let loc = i + "" + j;
+                let theBox = tempNumBoxArr[tempNumBoxArr.findIndex(pos => pos.x + "" + pos.y == loc)]
                 if (theBox) {
-                    console.log(theBox)
                     Object.assign(theBox, { y: theBox.y - space })
-                    console.log(theBox)
+                    animateMovement(loc, 0, -space);
                 } else space++
             }
         }
+        await waitExc(2000)
         setNumBoxArr(tempNumBoxArr);
         setmoved(moved + 1);
     }
 
     //funtion to move all the box to the right 
-    const moveRight = () => {
+    const moveRight = async () => {
         console.log("move right")
         let tempNumBoxArr = numBoxArr;
         for (let i = 3; i >= 0; i--) {
             let space = 0;
             for (let j = 3; j >= 0; j--) {
-                let theBox = tempNumBoxArr[tempNumBoxArr.findIndex(pos => pos.x + "" + pos.y == i + "" + j)]
+                let loc = i + "" + j;
+                let theBox = tempNumBoxArr[tempNumBoxArr.findIndex(pos => pos.x + "" + pos.y == loc)]
                 if (theBox) {
-                    console.log(theBox)
                     Object.assign(theBox, { y: theBox.y + space })
-                    console.log(theBox)
+                    animateMovement(loc, 0, space);
                 } else space++
             }
         }
+        await waitExc(2000)
         setNumBoxArr(tempNumBoxArr);
         setmoved(moved + 1);
     }
@@ -173,12 +184,12 @@ const page = () => {
             <div className='w-screen h-screen flex flex-col justify-evenly items-center'>
                 <header>The 2048</header>
                 {(startGameState === 0) ? <Menu start={setUpGame} /> :
-                    <main className='game-board w-2/3 h-2/3 flex flex-wrap relative'>
+                    <main className='game-board w-[65vw] h-[65vw] max-w-[600px] max-h-[600px] flex flex-wrap relative'>
                         {(moved) ?
                             boxes.map((ele, index) => {
                                 return (
                                     <div className={styles.box} >
-                                        {numBoxArr.some(index => index.x + "" + index.y === ele) ? <NumBox number={2} id={ele} /> : null}
+                                        {numBoxArr.some(index => index.x + "" + index.y === ele) ? <NumBox number={2} boxId={ele} /> : null}
                                     </div>
                                 );
                             }) : "no movements"
