955c4c1411be436d66d732985581e13b659d0d58
unknown <roshananand2005@gmail.com>
2024-11-02 11:51:35 +0530

    designed the final answer column

--- app/wordly-game/page.js
+++ app/wordly-game/page.js
@@ -1,18 +1,23 @@
 "use client"
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import { MyContext } from '@/Helper/Context'
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { MyContext } from '@/Helper/Context';
 import { Search } from 'lucide-react';
 import axios from 'axios';
 import gsap from 'gsap';
-import styles from "./wordly.module.css"
+import styles from "./wordly.module.css";
 
 const page = () => {
+
+
+  // https://api.datamuse.com/words?rel_jjb=(anyword)
+  // https://api.datamuse.com/words?ml=(anyword) ->to find words
 
   //context API
   const { screenUp, waitExc, startGameState, setstartGameState } = useContext(MyContext);
 
   const [answer, setanswer] = useState("");
   const [starTyping, setstarTyping] = useState(true);
+  const [currCol, setcurrCol] = useState(1);
   const [focusNum, setfocusNum] = useState(0);
   const [errMsg, seterrMsg] = useState("");
 
@@ -20,6 +25,7 @@ const page = () => {
 
   const inputRef = useRef();
 
+  //to get a random word
   const srchWords = async () => {
     const srchWord = inputRef.current.value;
     if (!srchWord) return;
@@ -41,12 +47,29 @@ const page = () => {
     setstarTyping(true);
   }
 
-  useEffect(() => {
-    screenUp();
-    setstartGameState(0)
-  }, [])
-  // https://api.datamuse.com/words?rel_jjb=(anyword)
-  // https://api.datamuse.com/words?ml=(anyword) ->to find words
+  //to show the correct letter from the word
+  const showCorrectLetter = (arrOfWrods) => {
+    const answerArr = answer.split('');
+    arrOfWrods.forEach((ele, index) => {
+      if (ele.value == answerArr[index]) {
+        ele.style.backgroundColor = "#98FB98";
+      }
+    })
+  }
+
+  //to check the current word is valid or not
+  const checkValidWord = async (word, arrOfWrods) => {
+    const res = await axios.get(`https://api.datamuse.com/words?rel_jjb=${word}`)
+    if (res.data.length == 0) {
+      popUpErrMsg("Invalid word");
+      arrOfWrods.forEach((ele) => {
+        ele.value = '';
+      })
+      setfocusNum(0);
+      return
+    }
+    showCorrectLetter(arrOfWrods);
+  }
 
   //to pop error message
   const popUpErrMsg = (msg) => {
@@ -56,13 +79,31 @@ const page = () => {
     }, 2000);
   }
 
+  //to check the given letters
+  const checkGivenLetters = () => {
+    let arrOfWrods = document.querySelectorAll(`.ans-input-${currCol}`)
+    let fullWords = "";
+    arrOfWrods.forEach((ele) => {
+      fullWords += ele.value;
+    })
+    console.log(fullWords);
+    checkValidWord(fullWords, arrOfWrods);
+  }
+
   //to get the value from input box
   const getInputBoxValue = () => {
     if (currInputBox.value == "") return
     const wordPattern = /^[A-Za-z]+$/;
+
     if (!wordPattern.test(currInputBox.value)) {
       currInputBox.value = '';
       popUpErrMsg("Only alphabets are allowed")
+      return
+    }
+
+    if (focusNum == 4) {
+      checkGivenLetters();
+      console.log("done");
       return
     }
     setfocusNum(prevNum => prevNum + 1);
@@ -71,25 +112,27 @@ const page = () => {
   //to remove the word from current box
   const backSpace = (e) => {
     if (e.key == 'Backspace') {
-      console.log("hai");
-
       if (focusNum == 0) {
         currInputBox.value = ''
         return
       }
       setfocusNum(prevNum => prevNum - 1);
-      // currInputBox = document.querySelectorAll('.ans-input')[focusNum];
     }
   }
 
+  //setup of the box after focus
   useEffect(() => {
-    const setFocus = () => { currInputBox.focus() }
+    const setFocus = (e) => {
+      let currEle = e.target.className;
+      if (currEle == 'final-ans-input' || currEle == 'wordly_answer-box-container__KJwkB') return
+      currInputBox.focus()
+    }
 
-    currInputBox = document.querySelectorAll('.ans-input')[focusNum];
+    currInputBox = document.querySelectorAll(`.ans-input-${currCol}`)[focusNum];
+    if (!currInputBox) return
     currInputBox.focus();
     currInputBox.addEventListener('input', getInputBoxValue)
     window.addEventListener('keydown', backSpace)
-    window.addEventListener("mouseover", setFocus)
     window.addEventListener("mousemove", setFocus)
     return () => {
       currInputBox.removeEventListener('input', getInputBoxValue)
@@ -97,7 +140,13 @@ const page = () => {
       window.removeEventListener("mouseover", setFocus)
       window.removeEventListener("mousemove", setFocus)
     }
-  }, [focusNum])
+  }, [focusNum, starTyping])
+
+  //initial setup
+  useEffect(() => {
+    screenUp();
+    setstartGameState(0)
+  }, [])
 
   return (
     <>
@@ -116,53 +165,54 @@ const page = () => {
               <input type='text' placeholder='Type any thing' className=' bg-transparent outline-none' ref={inputRef} />
               <button onClick={srchWords}><Search /></button>
             </div> :
-              <section className='flex flex-col'
-                style={{
-                  width: "min(80vw,650px)",
-                  height: "min(80vw,650px)",
-                }}>
+              <>
+                <section className={styles['main-boxes-container']}>
 
-                <div className={styles['input-box-container']}>
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                </div>
+                  <div className={styles['input-box-container']}>
+                    <input type='text' className='ans-input-1' />
+                    <input type='text' className='ans-input-1' />
+                    <input type='text' className='ans-input-1' />
+                    <input type='text' className='ans-input-1' />
+                    <input type='text' className='ans-input-1' />
+                  </div>
+                  <div className={styles['input-box-container']}>
+                    <input type='text' className='ans-input-2' />
+                    <input type='text' className='ans-input-2' />
+                    <input type='text' className='ans-input-2' />
+                    <input type='text' className='ans-input-2' />
+                    <input type='text' className='ans-input-2' />
+                  </div>
+                  <div className={styles['input-box-container']}>
+                    <input type='text' className='ans-input-3' />
+                    <input type='text' className='ans-input-3' />
+                    <input type='text' className='ans-input-3' />
+                    <input type='text' className='ans-input-3' />
+                    <input type='text' className='ans-input-3' />
+                  </div>
+                  <div className={styles['input-box-container']}>
+                    <input type='text' className='ans-input-4' />
+                    <input type='text' className='ans-input-4' />
+                    <input type='text' className='ans-input-4' />
+                    <input type='text' className='ans-input-4' />
+                    <input type='text' className='ans-input-4' />
+                  </div>
+                  <div className={styles['input-box-container']}>
+                    <input type='text' className='ans-input-5' />
+                    <input type='text' className='ans-input-5' />
+                    <input type='text' className='ans-input-5' />
+                    <input type='text' className='ans-input-5' />
+                    <input type='text' className='ans-input-5' />
+                  </div>
+                  <div className={styles['answer-box-container']}>
+                    <input type='text' className='final-ans-input' />
+                    <input type='text' className='final-ans-input' />
+                    <input type='text' className='final-ans-input' />
+                    <input type='text' className='final-ans-input' />
+                    <input type='text' className='final-ans-input' />
+                  </div>
 
-                <div className={styles['input-box-container']}>
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                </div>
-
-                <div className={styles['input-box-container']}>
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                </div>
-
-                <div className={styles['input-box-container']}>
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                  <input type='text' className='ans-input' />
-                </div>
-
-                <div className={styles['input-box-container']}>
-                  <input type='text' />
-                  <input type='text' />
-                  <input type='text' />
-                  <input type='text' />
-                  <input type='text' />
-                </div>
-
-              </section>
+                </section>
+              </>
           }
           <p>{answer}</p>
           <p className='w-full text-center text-[var(--footer-color)]'>{errMsg}</p>
